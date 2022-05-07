e7995deddfcadc499f71e7a974df8e87cf6f1ad6
soezawhtet94 <soezawhtet.wd@gmail.com>
2022-05-08 01:43:14 +0630

    modify api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,62 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('/user',function(Request $request) {
+        return $request->user();
+    });
+    Route::get('/employee',function(Request $request) {
+        $emp = [
+            [
+                "name" => "Emp",
+                "email" => "ss@gmail.com",
+                "phone" => "09888888",
+            ],
+            [
+                "name" => "Emp",
+                "email" => "ss@gmail.com",
+                "phone" => "09888888",
+            ],
+            [
+                "name" => "Emp",
+                "email" => "ss@gmail.com",
+                "phone" => "09888888",
+            ],
+            [
+                "name" => "Emp",
+                "email" => "ss@gmail.com",
+                "phone" => "09888888",
+            ],
+        ];
+        return response()->json([
+            'status'=> "OK",
+            'employee'=> $emp
+        ]);
+    });
+    Route::get('/employee/{id}',function(Request $request,$id) {
+        $emp = [
+                "id" => $id,
+                "name" => "Emp",
+                "email" => "ss@gmail.com",
+                "phone" => "09888888",
+        ];
+        return response()->json([
+            'status'=> "OK",
+            'employee'=> $emp
+        ]);
+    });
+    Route::post('/employee-register',function(Request $request) {
+
+        $emp = [
+                "name" => $request->name,
+                "email" => $request->email,
+        ];
+        return response()->json([
+            'status'=> "OK",
+            'employee'=> $emp
+        ]);
+    });
 });
